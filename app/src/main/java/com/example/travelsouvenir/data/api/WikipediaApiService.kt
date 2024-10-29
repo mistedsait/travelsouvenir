e49786093cb08ca0070d55a8bf35dd882a026596
Miralem Masic <miralemmasic234@gmail.com>
2024-10-29 11:08:24 +0100

    Revert "First Release"
    This reverts commit 56f65486495c2e49490b45d24c88e1a06fcaa3ba.

--- app/src/main/java/com/example/travelsouvenir/data/api/WikipediaApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.travelsouvenir.data.api
-
-import retrofit2.http.GET
-import retrofit2.http.Path
-
-interface WikipediaApiService {
-    @GET("page/summary/{title}")
-    suspend fun getPlaceInfo(@Path("title") title: String): WikipediaPlaceResponse
-}
